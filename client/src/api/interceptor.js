ed94bace4dd108694eebbfb4de01ff27bd82299d
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-11-01 15:21:53 +0200

    Fixed interceptors.request

--- client/src/api/interceptor.js
+++ client/src/api/interceptor.js
@@ -2,28 +2,31 @@ import axios from 'axios';
 import CONTANTS from '../constants';
 import history from '../browserHistory';
 
-const instance = axios.create({
+const httpClient = axios.create({
   baseURL: CONTANTS.BASE_URL,
 });
 
-instance.interceptors.request.use((config) => {
-  const token = window.localStorage.getItem(CONTANTS.ACCESS_TOKEN);
-  if (token) {
-    config.headers = { ...config.headers, 'Authorization':`Bearer ${token}` };
+let accessToken = null;
+
+httpClient.interceptors.request.use((config) => {
+  if (accessToken) {
+    config.headers = { ...config.headers, 'Authorization':`Bearer ${accessToken}` };
   }
   return config;
 }, (err) => Promise.reject(err));
 
-instance.interceptors.response.use((response) => {
-  if (response.data.token) {
-    window.localStorage.setItem(CONTANTS.ACCESS_TOKEN, response.data.token);
+httpClient.interceptors.response.use((response) => {
+  if (response.data.data.tokenPair) {
+    const {data: {data : {tokenPair: {access, refresh}}}} = response;
+    window.localStorage.setItem(CONTANTS.REFRESH_TOKEN, refresh);
+    accessToken = access;
   }
   return response;
 }, (err) => {
-  if (err.response.status === 408 && history.location.pathname !== '/login' && history.location.pathname !== '/registration' && history.location.pathname !== '/') {
-    history.replace('/login');
-  }
+  // if (err.response.status === 408 && history.location.pathname !== '/login' && history.location.pathname !== '/registration' && history.location.pathname !== '/') {
+  //   history.replace('/login');
+  // }
   return Promise.reject(err);
 });
 
-export default instance;
+export default httpClient;
